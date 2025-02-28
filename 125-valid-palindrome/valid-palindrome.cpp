82a6f92405d42c0c81d8869e5dfc2762ea71605f
techSiddhartha <dubeysiddhartha03@gmail.com>
2025-02-28 14:56:28 +0530

    Time: 4 ms (17.83%) | Memory: 10.9 MB (6.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,17 +3,17 @@
 
 class Solution {
 public:
-    bool isPalindrome(std::string s) {
-        std::string normalized_str;
+    bool isPalindrome(string s) {
+        string normalized_str;
     
         for (char c : s) {
-            if (std::isalnum(c)) {
-                normalized_str += std::tolower(c);
+            if (isalnum(c)) {
+                normalized_str += tolower(c);
             }
         }
         
 
-        std::string reversed_str = std::string(normalized_str.rbegin(), normalized_str.rend());
+        string reversed_str = string(normalized_str.rbegin(), normalized_str.rend());
         return normalized_str == reversed_str;
     }
 };
